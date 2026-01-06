f1cf26cef8b74776e5ec8113a152c9fb8456b8b7
Yuntong Qu <yuntong.qu@gmail.com>
2026-01-05 23:12:32 -0500

    Initial Commit

---
+++ HomeIndexTests/HomeIndexTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeIndexTests.swift
+//  HomeIndexTests
+//
+//  Created by Yuntong Qu on 1/5/26.
+//
+
+import Testing
+@testable import HomeIndex
+
+struct HomeIndexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
